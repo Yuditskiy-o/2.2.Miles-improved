7b246ad9f15e85dbeaf6bb2fc70e729c4a6d3efa
Yuditskiy-o <Komekadze@gmail.com>
2020-07-15 20:13:32 +0700

    Main and BonusMilesService modified

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,5 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-        cost = 6_500;
         int milefactor = 20;
         int miles = cost / milefactor;
         return miles;
